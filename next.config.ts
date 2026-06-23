bd5fc8261e5f941732a2c66e162a0a909ad421ae
hasanqazi-devloper <hasanqazi42@gmail.com>
2026-06-24 01:46:06 +0500

    fix: add netlify.toml catch-all redirects to resolve live 404 error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   }
+  // Standalone agar likha hai toh use temporarily hata do ya file ko aisi clean rakho!
 };
 
 export default nextConfig;
